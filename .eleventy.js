cb8656624dda2aead5ab42401013cc1d41178116
pgm-cagravci <cagravci@student.arteveldehs.be>
2021-04-06 19:27:07 +0200

    filter selected highlighted blogs

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,10 @@ const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
+    eleventyConfig.addFilter('highlight', function (blog) {
+        return blog.filter(blog => blog.data.highlight);
+    })
+
     eleventyConfig.addPassthroughCopy({
         'src/main.css': 'assets/main.css',
         'src/images': 'assets/img',
--- views/blogs/new-app.md
+++ views/blogs/new-app.md
@@ -4,6 +4,7 @@ permalink: "news/we-have-developed-a-new-walking-app/"
 title: Chineese new year - the year of the dog
 subtitle: "it's chinese new year this week. welcome to the year of the dog! weve taken a look at the 12 animals of the chinese zodiac and suggested a tur for each"
 img: gps.jpg
+highlight: true
 ---
 
 <article class="container">
